f8091205893c69d2a2f05490e6f3e4bfaff35e92
Marko <marko.kurtovic99@gmail.com>
2022-01-08 22:33:01 +0100

    create new car

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ const db = mongojs('marko', ['cars']);
 
 const app = express()
 app.set('view engine', 'ejs');
+//input parser
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req,res)=>{
     db.cars.find((err,data)=>{
         res.render('index', {data:data});
     });
+})
+
+app.get('/add',(req,res)=>{
+    res.render('add-view');
+})
+
+app.post('/save', (req,res)=>{
+    db.cars.insert({
+        name : req.body.name,
+        price : req.body.price,
+        used : req.body.used
+    },(err,data)=>{ 
+        res.redirect('/')
+    })
 })
 app.listen(3000, () =>{
     console.log('listening to port 3000');
